96f8ef64d3a799c3b4987a22b9e544406562232d
saujas <saujas.adarkar@gmail.com>
2019-02-27 23:33:35 +0530

    Lab1 tests and Lab sheets

--- Lab1/Solution/main.c
+++ Lab1/Solution/main.c
@@ -7,16 +7,16 @@ int main(void)
  seqarr seq;
  joblist arr;
  initialize_elements (arr);
- //printjoblist(arr);
+ printjoblist(arr);
  seq[0] = createlist();
  seq[1] = createlist();
  seq[2] = createlist();
  insertelements (arr,seq);
-// printseqlist (seq[0]);
-// printseqlist (seq[1]);
-// printseqlist (seq[2]);
+ printseqlist (seq[0]);
+ printseqlist (seq[1]);
+ printseqlist (seq[2]);
  copy_sorted_ele (seq , arr); 
- //printf ("Sorted List\n");
+ printf ("Sorted List\n");
  printjoblist(arr);
 }
  
