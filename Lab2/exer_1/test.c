9be19d879af64867aab56e37995fc5d311bbfd5b
saujas <saujas.adarkar@gmail.com>
2019-02-27 23:37:16 +0530

    Lab 2 - extract file name fixed

--- Lab2/exer_1/test.c
+++ Lab2/exer_1/test.c
@@ -5,22 +5,24 @@
 #include<string.h>
 int main(int argc, char *argv[])
 {
-	int c = sizeof(argv[0])/sizeof(argv[0][0]);
 	int n;
-	c--;
-	for(int i = 0; i<c;i++)
+	int c = 2;
+	char name[sizeof(argv[0])];
+	
+	while(c < sizeof(argv[0]))
 	{
-		if (i==0 || i==1)
-			continue;
-		printf("%c", argv[0][i]);
+		name[c-2] = argv[0][c];
+		c++;
 	}
-	printf(" ");
+
+	printf("%s ", name);
+
 	for(int i = 1; i<argc; i++)
 	{
 		if(atoi(argv[i]))
 		{
 			n = atoi(argv[i]);
-			printf("%d (int) ", n);
+			printf("%d(int) ", n);
 			continue;
 		}
 		printf("%s ", argv[i]);
